f90cb3d12a5a443df2ab514ebb506ad6e057c19e
Chin <chinehuu@gmail.com>
2022-05-01 11:13:38 -0700

    initial commit

---
+++ client/src/PortfolioContainer/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css';
+
+export function Footer() {
+    return (
+        <div className='footer-container'>
+            <div className='footer-parent'>
+                <img src={require('../../assets/Home/ChinResume.jpg')} alt='No picture'/>
+            </div>
+        </div>
+    )
+}
---
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -0,0 +1,9 @@
+import Recat from 'react'
+
+export default function Resume() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
